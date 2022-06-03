d3e4b6a5d5216e00f668fb24551aa14c3b74e718
DanielSTS <daniel.junior.ifpb@gmail.com>
2022-06-02 22:09:04 -0300

    Adjust

---
+++ src/main/java/br/com/gsb-solutions/GsbSolutionsApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetocrud.projetocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsbSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(br.com.projetocrud.projetocrud.GsbSolutionsApplication.class, args);
+	}
+
+}
